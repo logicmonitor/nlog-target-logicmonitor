3462965a30db2997381919be1f4b3cb306e1224b
Aakash Khopade <aakash.khopade@logicmonitor.com>
2023-05-08 09:52:37 +0530

    NLog appendder
    Send Logs to Lm platform
    configure via xml file

---
+++ NLog.Targets.LogicMonitor/LogicMonitorTarget.cs
@@ -0,0 +1,104 @@
+﻿using LogicMonitor.DataSDK.Api;
+using LogicMonitor.DataSDK.Model;
+using LogicMonitor.DataSDK;
+using System.Diagnostics;
+using NLog.Layouts;
+namespace NLog.Targets.LogicMonitor
+{
+
+    [Target("LogicMonitor")]
+    public class LogicMonitorTarget : TargetWithContext
+    {
+        public Layout Interval { get; set; }
+        public Layout Batch { get; set; }
+        public Layout Company { get; set; }
+        public Layout LMAccessKey { get; set; }
+        public Layout LMAccessID { get; set; }
+        public Layout ResourceName { get; set; }
+
+        private Configuration _config;
+        private ApiClient _apiClient;
+        private Logs _logs;
+        private Resource _resource;
+        private int _interval { get; set; }
+        private bool _batch { get; set; }
+
+        public LogicMonitorTarget() : base()
+        {
+            _interval = 10;
+            _batch = true;
+        }
+
+        public LogicMonitorTarget(Configuration configuration, Resource resource = null, int interval = 10, bool batch = true) : base()
+        {
+            _config = configuration ??= new Configuration();
+            _interval = interval;
+            _batch = batch;
+            _resource = resource;
+        }
+        protected override void InitializeTarget()
+        {
+            base.InitializeTarget();
+            string company = Company.ToString();
+            string lmAccessID = LMAccessID.ToString();
+            string lmAccessKey = LMAccessKey.ToString();
+            _batch = Convert.ToBoolean(Batch.ToString());
+            _interval = Convert.ToInt32(Interval.ToString());
+            _config = new Configuration(company: company, accessID: lmAccessID, accessKey: lmAccessKey);
+            _resource = new Resource();
+            if (ResourceName != null)
+            {
+                _resource.Name = ResourceName.ToString();
+                _resource.Ids = new Dictionary<string, string>();
+                _resource.Ids.Add("system.hostname", ResourceName.ToString());
+            }
+            _apiClient = new ApiClient(configuration: _config ??= new Configuration());
+
+            _logs = new Logs(batch: _batch, interval: _interval, apiClient: _apiClient);
+
+        }
+
+        protected override void CloseTarget()
+        {
+            base.CloseTarget();
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            try
+            {
+                Dictionary<string, string> metaData = new Dictionary<string, string>();
+                string formattedMessage = logEvent.FormattedMessage;
+                if (Activity.Current != null)
+                    metaData = LogEventMetaData(Activity.Current);
+
+                var currentLogLevel = logEvent.Level.ToString();
+                metaData.Add(Constants.LogLevel, currentLogLevel);
+
+                DateTimeOffset milliseconds = logEvent.TimeStamp;
+                long tst = milliseconds.ToUnixTimeMilliseconds();
+                _logs.SendLogs(message: formattedMessage, resource: _resource, metadata: metaData, timestamp: tst);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+            }
+        }
+
+        private Dictionary<string, string> LogEventMetaData(Activity activity)
+        {
+
+            var traceID = activity.TraceId.ToString();
+            var spanId = activity.SpanId.ToString();
+            var operationName = activity.OperationName.ToString();
+
+            Dictionary<string, string> keyValues = new Dictionary<string, string>();
+            keyValues.Add(Constants.TracesKey.TraceId, traceID);
+            keyValues.Add(Constants.TracesKey.SpanId, spanId);
+            keyValues.Add(Constants.TracesKey.OperationName, operationName);
+            return keyValues;
+        }
+
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# nlog-target-logicmonitor
-NLOG.Target.LogicMonitor
+# NLog.Targets.LogicMonitor
+
+A NLog Target that send events and logs directly to LogicMonitor platform.
+This Package leverages LogicMonitor.DataSDK to send logs.
+
+Export the following environment variable.
+
+ System property   |      Environment variable      |  Description |
+|----------|-------------|:------|
+| `Configration.company` |  `LM_COMPANY` |  Account name (Company Name) is your organization name |
+| `Configration.AccessID` |  `LM_ACCESS_ID` |  Access id while using LMv1 authentication. (Not needed while using Bearer API )  |
+| `Configration.AccessKey` |  `LM_ACCESS_KEY` |    Access key while using LMv1 authentication. (Not needed while using Bearer API ) |
+
+```csharp
+        var logger = NLog.LogManager.GetCurrentClassLogger();
+        var config = new NLog.Config.LoggingConfiguration();
+
+        // Targets where to log to: Console
+        var logMonitor = new NLog.Targets.LogicMonitor.LogicMonitorTarget();
+
+        // Rules for mapping loggers to targets
+        config.AddRule(LogLevel.Info, LogLevel.Error, logMonitor);
+
+        // Apply config
+        NLog.LogManager.Configuration = config;
+```
+
+or 
+
+:point_right: Learn more about [NLog Config](https://github.com/nlog/nlog/wiki/Configuration-file) 
+
+```csharp
+//Note.: Add a NLog.Config.xml to your project.
+
+var logger = NLog.LogManager.Setup().LoadConfigurationFromXml("Nlog.Config").GetCurrentClassLogger();
+
+```
+
